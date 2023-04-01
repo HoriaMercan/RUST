5bf6b793e6304e40b9207dc18f58fd9f2c66482a
Horia Mercan <horia.mercan@gmail.com>
2023-04-01 15:15:56 +0300

    first_commit

--- src/complex.rs
+++ src/complex.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 struct Complex {
     real: f64,
     imaginary: f64,
@@ -5,36 +6,119 @@ struct Complex {
 
 impl Complex {
     // TODO 1 - implement run
+    fn new(_real: f64, _imaginary: f64) -> Complex {
+        Complex {
+            real: _real,
+            imaginary: _imaginary,
+        }
+    }
 }
+
+use std::fmt;
+use std::ops::{Add, Sub};
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        
+        let ans = match (self.real, self.imaginary) {
+            (ref x , ref y) if *x == 0.0 && *y == 0.0 => write!(f, "0"),
+            (ref x, ref y) if *y == 0.0 => write!(f, "{}", x),
+            (ref x, ref y) if *x == 0.0 => write!(f, "{}i", y),
+            (ref x, ref y) if *y < 0.0 => write!(f, "{}{}i", x, y),
+            (ref z, ref t) => write!(f, "{}+{}i", z, t),
+        };
+
+        ans
+    }
+}
+
+impl PartialEq for Complex {
+    fn eq(&self, other: &Self) -> bool {
+        self.real == other.real && self.imaginary == other.imaginary
+    }
+}
+
+fn module(x : &Complex) -> f64 {
+    let ans = x.real * x.real + x.imaginary * x.imaginary;
+    let ans = ans.sqrt();
+    ans
+}
+
+
+impl PartialOrd for Complex {
+    
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let x = module(&self);
+        let y = module(&other);
+        x.partial_cmp(&y)
+    }
+    
+    fn ge(&self, other: &Self) -> bool {
+        module(&self) >= module(&other)
+    }
+    
+    fn gt(&self, other: &Self) -> bool {
+        module(&self) > module(&other)
+    }
+
+    fn le(&self, other: &Self) -> bool {
+        module(&self) <= module(&other)
+    }
+
+    fn lt(&self, other: &Self) -> bool {
+        module(&self) < module(&other)
+    }
+}
+
+impl Copy for Complex {}
+
 
 // TODO 2 - implement the Add, Sub, Mul and Div traits
 
+impl Add for Complex {
+
+    type Output = Self;
+    fn add(self, other: Complex) -> Self::Output {
+        let ans = Complex::new(self.real + other.real, self.imaginary + other.imaginary);
+        ans
+    }
+}
+
+impl Sub for Complex {
+
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        let ans = Complex::new(self.real - other.real, self.imaginary - other.imaginary);
+        ans
+    }
+}
+
 pub fn run() {
-    // let n1 = Complex::new(2, 3);
-    // let n2 = Complex::new(-2, 3);
-    // let n3 = Complex::new(2, -3);
-    // let n4 = Complex::new(3, 0);
-    // let n5 = Complex::new(0, 3);
-    // let n7 = Complex::new(2, 3);
-    // println!("The number is {}", n1); // prints 2+3i
-    // println!("The number is {}", n2); // prints -2+3i
-    // println!("The number is {}", n3); // prints 2-3i
-    // println!("The number is {}", n4); // prints 3
-    // println!("The number is {}", n5); // prints 3i
+    let n1 = Complex::new(2 as f64, 3 as f64);
+    let n2 = Complex::new(-2 as f64, 3 as f64);
+    let n3 = Complex::new(2 as f64, -3 as f64);
+    let n4 = Complex::new(3 as f64, 0 as f64);
+    let n5 = Complex::new(0 as f64, 3 as f64);
+    let n7 = Complex::new(2 as f64, 3 as f64);
+    println!("The number is {}", n1); // prints 2+3i
+    println!("The number is {}", n2); // prints -2+3i
+    println!("The number is {}", n3); // prints 2-3i
+    println!("The number is {}", n4); // prints 3
+    println!("The number is {}", n5); // prints 3i
 
-    // println!("The number is {}", n1 - n7); // prints 0
-    // println!("The number is {}", n1 + n2);
-    // println!("The number is {}", n1 - n2);
+    println!("The number is {}", n1 - n7); // prints 0
+    println!("The number is {}", n1 + n2);
+    println!("The number is {}", n1 - n2);
 
-    // println!(
-    //     "The numbers {} and {} are {}",
-    //     n1,
-    //     n2,
-    //     if n1 == n2 { "equal" } else { "not equal" }
-    // );
+    println!(
+        "The numbers {} and {} are {}",
+        n1,
+        n2,
+        if n1 == n2 { "equal" } else { "not equal" }
+    );
 
-    // println!("The number is {}", n1 < n2);
-    // println!("The number is {}", n1 <= n2);
-    // println!("The number is {}", n1 > n2);
-    // println!("The number is {}", n1 >= n2);
+    println!("The number is {}", n1 < n2);
+    println!("The number is {}", n1 <= n2);
+    println!("The number is {}", n1 > n2);
+    println!("The number is {}", n1 >= n2);
 }
--- src/errors.rs
+++ src/errors.rs
@@ -1,26 +1,63 @@
 // TODO 1 - fix this function
-// pub fn divide(a: i32, b: i32) -> Option<isize> {
-//     a/b
-// }
+pub fn divide(a: i32, b: i32) -> Option<isize> {
+    if b == 0 {
+        None
+    } else {
+        Some((a / b) as isize)
+    }
+}
 
 // TODO 3 - fix this function
-// pub fn divide_error(a: i32, b: i32) -> Result<isize, String> {
-    
-//     a/b
-// }
+pub fn divide_error(a: i32, b: i32) -> Result<isize, String> {
+    match divide(a, b) {
+        None => Err(String::from("De ce imparti la 0?")),
+        Some(c) => Ok(c)
+    }
+}
 
 // TODO 5 - list files in the diretcory
 //  - if the directory does not exist, return None
-// fn list_dir(path: &str) -> Option<Vec<String>> {
+use std::{fs, path::Path};
+use std::vec::Vec;
 
-// }
+fn list_dir(path: &str) -> Option<Vec<String>>{
+
+    if Path::new(path).exists() {
+
+        let paths = fs::read_dir(path).unwrap();
+
+        let mut vector = Vec::new(); 
+
+        for __path in paths {
+            vector.push(__path.unwrap().path().as_path().display().to_string());
+        }
+
+        Some(vector)
+
+    } else {
+        None
+    }
+}
 
 pub fn run() {
     // TODO 2 - make the print work, use match and/or if let
     // println!("division: ", divide(a, b));
+    match divide(32, 3) {
+        None => println!("Don't divide by 0"),
+        Some(c) => println!("division result : {}", c),
+    }
+    
 
     // TODO 4 - make the print work, use match and/or if let
     // println!("division: ", divide_error(a, b));
+    let new_div = divide_error(10, 0);
+    match new_div {
+        Ok(c) => println!("division: {}", c),
+        Err(err) => println!("{}", err),
+    }
 
     // TODO 6 - use the list_directory function to print the current directory
+    let path = Path::new("./");
+    // list_dir("./")
+    // list_dir("./") 
 }
